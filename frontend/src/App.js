6f84b3c4050be9fdfe0c8d58d703785f7fae8751
X64 <alberto.arias.x64@gmail.com>
2022-06-30 08:13:56 -0500

    new frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <header>
+        <h1>This is my To do APP </h1>
+        <p>Develop release 1.0.0</p>
+        <div class="spiner"></div>
+      </header>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <header>
+      <h1>This is my To do APP </h1>
+      <p>Develop release 1.0.0</p>
+      <div class="spiner"></div>
+    </header>
+  </>
+)
